630c8876c45d5c00c1116b8d636976700dc3c242
Christopher Wood <christopherwood07@gmail.com>
2015-01-25 14:01:43 -0800

    more android stuff

--- app/ImpGlow/app/src/main/java/cawcode/cwood/impglow/MainActivity.java
+++ app/ImpGlow/app/src/main/java/cawcode/cwood/impglow/MainActivity.java
@@ -2,11 +2,15 @@ package cawcode.cwood.impglow;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "cawcode.cwood.impglow.MESSAGE"; // key for intent object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,15 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     @Override
